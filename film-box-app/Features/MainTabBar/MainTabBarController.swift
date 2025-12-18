b1ec05f63fe10140f552b14c69a6bda801bb2f63
Geovane Lima <geovanelima@192.168.0.21>
2025-12-18 20:17:21 -0300

    Initial commit Film Box App

---
+++ film-box-app/Features/MainTabBar/MainTabBarController.swift
@@ -0,0 +1,12 @@
+//
+//  MainTabBarController.swift
+//  film-box-app
+//
+//  Created by Geovane Lima on 18/12/25.
+//
+
+import UIKit
+
+final class MainTabBarController: UITabBarController {
+    
+}
